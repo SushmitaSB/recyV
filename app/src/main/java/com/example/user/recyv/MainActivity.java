93bfff2ff4783fbcf0aed9cec21cffcab9d9be29
Mellow corp <SushmitaSB@bitbucket.org>
2018-09-27 23:20:37 -0700

    third commit

--- app/src/main/java/com/example/user/recyv/MainActivity.java
+++ app/src/main/java/com/example/user/recyv/MainActivity.java
@@ -2,22 +2,36 @@ package com.example.user.recyv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.text.InputType;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView rv;
-    String s[]={"a","b"};
+   CheckBox chk;
+   EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rv.findViewById(R.id.rvid);
-        rv.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
-        rv.setAdapter(new ca());
-        //hello
+
+        chk=findViewById(R.id.etchk);
+        et=findViewById(R.id.etpass);
+        chk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                {
+                    et.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                }
+                else
+                {
+                    et.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                }
+            }
+        });
 
     }
-    public class ca extends RecyclerView.Adapter
+
 }
